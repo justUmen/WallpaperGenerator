0eb64da1c62f74cd285e8f56352d567fb23af368
justumen <me@justumen.org>
2018-08-29 12:28:05 +0700

    i3wm1

--- wallpaper_generator.sh
+++ wallpaper_generator.sh
@@ -3,23 +3,31 @@
 BG_COLOR="black"
 FG_COLOR="white"
 
+#~ BG_COLOR="white"
+#~ FG_COLOR="black"
 
 #~ LANGUAGE="en"
 #~ FILES="DB/$LANGUAGE/i3wm_configuration.txt XxX DB/$LANGUAGE/i3wm_default_shortcuts.txt"
 #~ IMAGE="DB/image/i3wm.png"
 #~ NAME="i3wm"
 
+#~ LANGUAGE="fr"
+##~ FILES="DB/$LANGUAGE/bash_1.txt"
+#~ FILES="DB/$LANGUAGE/QUIZ_BASH_1_3"
+#~ IMAGE="DB/image/bash.png"
+#~ NAME="quiz_bash_1_3"
+
 LANGUAGE="fr"
-FILES="DB/$LANGUAGE/bash_1.txt"
-IMAGE="DB/image/bash.png"
-NAME="bash_1"
+FILES="DB/$LANGUAGE/i3wm_1"
+IMAGE="DB/image/i3wm.png"
+NAME="i3wm_1"
 
 mkdir Wallpaper/$LANGUAGE/$NAME 2> /dev/null
 
 NB_LINES=`cat $FILES | wc -l`
 
 # SCREEN_W=1366; SCREEN_H=768
-# SCREEN_W=1920; SCREEN_H=1080
+#~ SCREEN_W=1920; SCREEN_H=1080
 # SCREEN_W=800; SCREEN_H=600
 # SCREEN_W=1024; SCREEN_H=768
 
@@ -45,11 +53,12 @@ if [ ! "$IMAGE" == "" ]; then
   half_image=`expr $IMAGE_WIDTH / 2`
 fi
 
-if [ $NB_LINES -gt 30 ];  then #TWO COLUMNS
+MAX_LINES=30
+if [ $NB_LINES -gt $MAX_LINES ];  then #TWO COLUMNS
   line_width=`expr \( $SCREEN_W / 2 \) - $margin_left - $margin_left` #MArgin left / right
-  line_height=`expr $SCREEN_H / 30`
+  line_height=`expr $SCREEN_H / $MAX_LINES`
 else
-  line_width=$SCREEN_W
+  line_width=`expr $SCREEN_W - $margin_left - $margin_left - $margin_left`
   line_height=`expr \( $SCREEN_H - $IMAGE_HEIGHT - $margin_top \) / \( $NB_LINES + 2 \) ` # +5 lines
 fi
 im_X=`expr ${line_width} / 2 - $half_image`
@@ -70,7 +79,7 @@ cat $FILES | while read line; do
   varR=$(echo $line | cut -f2 -d:)
   y=`expr $y + ${line_height}`
   i_line=`expr $i_line + 1`
-  if [ "$i_line" == "30" ] || [ "$line" == "XxX" ]; then
+  if [ "$i_line" == "$MAX_LINES" ] || [ "$line" == "XxX" ]; then
     x=`expr $line_width + $margin_left + $margin_left` #MARGIN LEFT
     y=$margin_top
     i_line=0
@@ -79,14 +88,19 @@ cat $FILES | while read line; do
   fi
   x_margin=`expr $x + $margin_left`
   if [ ! "$line" == "XxX" ]; then
-    convert -fill $FG_COLOR -background $BG_COLOR -size ${line_width}x${line_height} -page +${x_margin}+${y} label:"${varL} = ${varR}"  miff:-
+	if [ ! "$line" == "" ]; then
+		convert -fill $FG_COLOR -background $BG_COLOR -size ${line_width}x${line_height} -page +${x_margin}+${y} label:"${varL} = ${varR}"  miff:-
+	else
+		convert -fill $FG_COLOR -background $BG_COLOR -size ${line_width}x${line_height} -page +${x_margin}+${y} label:"" miff:-
+	fi
   fi
-done | convert -background $BG_COLOR -size ${SCREEN_W}x${SCREEN_H} xc: - -flatten wallpaper.jpg
+done | convert -size ${SCREEN_W}x${SCREEN_H} xc:$BG_COLOR - -flatten wallpaper.jpg
 #CAN TEST WITH white background
 
 convert wallpaper.jpg tmpIM -geometry +${im_X}+${margin_top} -composite Wallpaper/$LANGUAGE/$NAME/${SCREEN_W}x${SCREEN_H}.jpg
-rm wallpaper.jpg
-rm tmpIM
+#rm wallpaper.jpg
+#rm tmpIM
+
 # feh --bg-scale out.jpg
 # viewnior ${SCREEN_W}x${SCREEN_H}.jpg &
 
