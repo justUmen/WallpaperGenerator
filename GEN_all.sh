bcae5c89a7f0673fb8c0463f014ca30bb49c09f9
justumen <me@justumen.org>
2018-09-26 13:14:04 +0700

    ../..

--- GEN_all.sh
+++ GEN_all.sh
@@ -3,6 +3,5 @@
 ./wallpaper_generator.sh 1366 768
 ./wallpaper_generator.sh 1360 768
 ./wallpaper_generator.sh 1024 768
-./wallpaper_generator.sh 800 600
 ./wallpaper_generator.sh 1900 900
 ./wallpaper_generator.sh 1280 1024
--- wallpaper_generator.sh
+++ wallpaper_generator.sh
@@ -1,44 +1,30 @@
 #!/bin/bash
+#MANUAL : Avoid long lines
+#MANUAL : Prepare logo position top right
+#< 12 lines : bigger font, logo top middle and text will be lower
+#> 12 lines : logo top right
+
+image_position="top_right"
+SUBJECT="i3wm"
+NUMBER="1"
+
+LANGUAGE="fr"
+FILES="DB/$LANGUAGE/$SUBJECT/_${NUMBER}.txt"
+IMAGE="DB/image/$SUBJECT.png"
+NAME="${SUBJECT}_${NUMBER}"
+mkdir Wallpaper/$LANGUAGE/$NAME 2> /dev/null
 
 BG_COLOR="black"
 FG_COLOR="white"
 
-#~ BG_COLOR="white"
-#~ FG_COLOR="black"
-
-#~ LANGUAGE="en"
-#~ FILES="DB/$LANGUAGE/i3wm_configuration.txt XxX DB/$LANGUAGE/i3wm_default_shortcuts.txt"
-#~ IMAGE="DB/image/i3wm.png"
-#~ NAME="i3wm"
-
-LANGUAGE="fr"
-#~ ##~ FILES="DB/$LANGUAGE/bash_1.txt"
-FILES="DB/$LANGUAGE/QUIZ_BASH_1_3"
-IMAGE="DB/image/bash.png"
-NAME="quiz_bash_1_3"
-
-#~ LANGUAGE="fr"
-#~ FILES="DB/$LANGUAGE/i3wm_1"
-#~ IMAGE="DB/image/i3wm.png"
-#~ NAME="i3wm_1"
-
-mkdir Wallpaper/$LANGUAGE/$NAME 2> /dev/null
-
 NB_LINES=`cat $FILES | wc -l`
 
-# SCREEN_W=1366; SCREEN_H=768
-#~ SCREEN_W=1920; SCREEN_H=1080
-# SCREEN_W=800; SCREEN_H=600
-# SCREEN_W=1024; SCREEN_H=768
-
-SCREEN_W=$1; SCREEN_H=$2
+SCREEN_W=$1
+SCREEN_H=$2
 
 #BASE 10,10 for 800x600
 margin_top=`expr $SCREEN_H / 60`
 margin_left=`expr \( $SCREEN_W / 80 \) / 2`
-
-#ONE LINE IS 25px + margin ?
-#MAX_LINES=
 
 limit_height_image=`expr $SCREEN_H / 3`
 
@@ -53,26 +39,56 @@ if [ ! "$IMAGE" == "" ]; then
   half_image=`expr $IMAGE_WIDTH / 2`
 fi
 
-MAX_LINES=30
+MAX_LINES=24
 if [ $NB_LINES -gt $MAX_LINES ];  then #TWO COLUMNS
   line_width=`expr \( $SCREEN_W / 2 \) - $margin_left - $margin_left` #MArgin left / right
   line_height=`expr $SCREEN_H / $MAX_LINES`
-else
+elif [ $NB_LINES -lt 12 ]; then
+  image_position="top_middle"
   line_width=`expr $SCREEN_W - $margin_left - $margin_left - $margin_left`
   line_height=`expr \( $SCREEN_H - $IMAGE_HEIGHT - $margin_top \) / \( $NB_LINES + 2 \) ` # +5 lines
+  #~ line_height=`expr \( $SCREEN_H - $margin_top \) / \( $NB_LINES \) ` # +5 lines
+else
+  line_width=`expr $SCREEN_W - $margin_left - $margin_left - $margin_left`
+  #~ line_height=`expr \( $SCREEN_H - $IMAGE_HEIGHT - $margin_top \) / \( $NB_LINES + 2 \) ` # +5 lines
+  line_height=`expr \( $SCREEN_H - $margin_top \) / \( $NB_LINES \) ` # +5 lines
 fi
 im_X=`expr ${line_width} / 2 - $half_image`
 
 
-echo $NB_LINES
-echo $pointsize
+echo " $NAME - ${SCREEN_W}x${SCREEN_H}"
+echo " NB_LINES = $NB_LINES"
+#~ echo $pointsize
 
 rm wallpaper.jpg 2> /dev/null
+
+
+FONT_SIZE=20000
+if [ $NB_LINES -lt 12 ]; then
+	case $2 in
+		1080) FONT_SIZE=30000 ;;
+		1024) FONT_SIZE=30000 ;;
+		900)  FONT_SIZE=25000 ;;
+		768)  FONT_SIZE=25000 ;;
+	esac
+else
+	case $2 in
+		1080) FONT_SIZE=25000 ;;
+		1024) FONT_SIZE=25000 ;;
+		900)  FONT_SIZE=20000 ;;
+		768)  FONT_SIZE=20000 ;;
+	esac
+fi
 
 i_line=0
 cat $FILES | while read line; do
   if [ ! "$IMAGE" == "" ]; then #IF WALLPAPER HAVE IMAGE
-    y=`expr $limit_height_image + $margin_top`
+	#Pas de decalage en fonction de l'image
+    if [ $NB_LINES -lt 12 ]; then
+		y=`expr $limit_height_image + $margin_top`
+    else
+		y=0
+    fi
     IMAGE=""
   fi
   varL=$(echo $line | cut -f1 -d:)
@@ -89,8 +105,8 @@ cat $FILES | while read line; do
   x_margin=`expr $x + $margin_left`
   if [ ! "$line" == "XxX" ]; then
 	if [ ! "$line" == "" ]; then
-		echo " --> ${line_width}x${line_height}   /   +${x_margin}+${y}" >&2
-		convert -fill "$FG_COLOR" -background "$BG_COLOR" pango:"<span foreground='white'>${varL}</span>     <span foreground='yellow'>${varR}</span>" -page +${x_margin}+${y} miff:-
+		#~ echo " --> ${line_width}x${line_height}   /   +${x_margin}+${y}" >&2
+		convert -fill "$FG_COLOR" -background "$BG_COLOR" -size "${line_width}x${line_height}" pango:"<span size='$FONT_SIZE' foreground='white'>${varL}</span>     <span size='$FONT_SIZE' foreground='yellow'>${varR}</span>" -page +${x_margin}+${y} miff:-
 	else
 		convert -fill "$FG_COLOR" -background "$BG_COLOR" -size "${line_width}x${line_height}" -page +${x_margin}+${y} label:"" miff:-
 	fi
@@ -98,9 +114,26 @@ cat $FILES | while read line; do
 done | convert -size ${SCREEN_W}x${SCREEN_H} xc:$BG_COLOR - -flatten wallpaper.jpg
 #CAN TEST WITH white background
 
-convert wallpaper.jpg tmpIM -geometry +${im_X}+${margin_top} -composite Wallpaper/$LANGUAGE/$NAME/${SCREEN_W}x${SCREEN_H}.jpg
-#rm wallpaper.jpg
-#rm tmpIM
+#IMAGE TOP MIDDLE
+#~ convert wallpaper.jpg tmpIM -geometry +${im_X}+${margin_top} -composite Wallpaper/$LANGUAGE/$NAME/${SCREEN_W}x${SCREEN_H}.jpg
+
+#IMAGE TOP RIGHT
+if [[ "$image_position" == "top_right" ]]; then
+	#BIGGER MARGIN TOP AND RIGHT... x5 ?
+	TMP_margin_top=$(expr $margin_top + $margin_top + $margin_top + $margin_top + $margin_top)
+	LEFT_IM=$(expr $SCREEN_W - $IMAGE_WIDTH - $margin_top - $margin_top - $margin_top - $margin_top - $margin_top )
+	echo "+ Position of image :  +${LEFT_IM}+${TMP_margin_top} (top_right)"
+	convert wallpaper.jpg tmpIM -geometry +${LEFT_IM}+${TMP_margin_top} -composite Wallpaper/$LANGUAGE/$NAME/${SCREEN_W}x${SCREEN_H}.jpg
+elif [[ "$image_position" == "top_middle" ]]; then
+	echo "+ Position of image :  +${im_X}+${margin_top} (top_middle)"
+	convert wallpaper.jpg tmpIM -geometry +${im_X}+${margin_top} -composite Wallpaper/$LANGUAGE/$NAME/${SCREEN_W}x${SCREEN_H}.jpg
+fi
+
+echo
+
+#CLEAN UP
+rm wallpaper.jpg
+rm tmpIM
 
 # feh --bg-scale out.jpg
 # viewnior ${SCREEN_W}x${SCREEN_H}.jpg &
